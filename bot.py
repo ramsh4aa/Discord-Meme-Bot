4e17f1a55bd85826fea78753b870e400e69114b8
Ramsha <ramshazubair@gmail.com>
2025-12-11 21:27:56 +0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # Prevent replying to itself
+        if message.author == self.user:
+            return
+
+        # Check for $hello command
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+# Set up intents (permissions)
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Create the bot client
+client = MyClient(intents=intents)
+
+# Start the bot (replace YOUR_TOKEN_HERE)
+client.run('your_token_here')
